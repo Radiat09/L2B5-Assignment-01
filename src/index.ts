92433e512c15276d3af26ff3549ff34c2b02e01c
Radiat09 <rzsianboyridoy10@gmail.com>
2025-05-07 20:56:00 +0600

    solve problem 4

--- src/index.ts
+++ src/index.ts
@@ -33,3 +33,43 @@ const b = [3, 4];
 const c = [5];
 
 // console.log(concatenateArrays(a, b, c));
+
+//***  Solution 4 ***/
+class Vehicle {
+  constructor(
+    private make: string,
+    private year: number,
+    public color: string,
+    public isRunning: boolean
+  ) {}
+
+  getInfo() {
+    return {
+      make: this.make,
+      year: this.year,
+      color: this.color,
+      isRunning: this.isRunning,
+    };
+  }
+}
+
+class Car extends Vehicle {
+  constructor(
+    make: string,
+    year: number,
+    color: string,
+    isRunning: boolean,
+    private model: string
+  ) {
+    super(make, year, color, isRunning);
+  }
+
+  getModel(): { model: string } {
+    return { model: this.model };
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Blackish Red", true, "Corolla");
+
+console.log(myCar.getInfo());
+console.log(myCar.getModel());
